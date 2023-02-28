112d798e8bf5c28266a0972dedb2144ad34bf777
Mohib <gazi.mohib@brainstation-23.com>
2023-02-28 20:49:27 +0000

    setup config server

---
+++ src/test/java/com/abc/bank/bankconfigserver/BankConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.bank.bankconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
